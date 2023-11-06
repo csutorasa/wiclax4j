1cc84c39f879e9f51ebbf964c14d6bff6bf97f6c
Ármin <csutorasa@gmail.com>
2023-11-06 14:31:25 +0100

    Change to kotlin script file

---
+++ build.gradle.kts
@@ -0,0 +1,107 @@
+repositories {
+    mavenCentral()
+}
+
+plugins {
+    id("java-library")
+    id("groovy")
+    id("maven-publish")
+    id("signing")
+    id("jacoco")
+    id("org.sonarqube").version("4.4.0.3356")
+}
+
+version = project.properties["wiclax4j.version"] as String
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    testImplementation("org.apache.groovy:groovy-all:4.0.15")
+    testImplementation("org.spockframework:spock-core:2.3-groovy-4.0")
+    testImplementation("net.bytebuddy:byte-buddy:1.14.8")
+    testImplementation("org.objenesis:objenesis:3.3")
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = project.properties["nexus.username"] as String?
+                password = project.properties["nexus.password"] as String?
+            }
+        }
+    }
+    publications {
+        create("mavenJava", MavenPublication::class.java) {
+            groupId = "com.github.csutorasa.wiclax4j"
+            artifactId = "wiclax4j"
+            version = project.version as String
+
+            from(components["java"])
+
+            pom {
+                name = "Wiclax4j"
+                description = "This is a pure java generic acquisition Wiclax server implementation"
+                url = "https://www.github.com/csutorasa/wiclax4j"
+                licenses {
+                    license {
+                        name = "The MIT License"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "csutorasa"
+                        name = "Ármin Csutorás"
+                        email = "csutorasa@gmail.com"
+                        organizationUrl = "https://github.com/csutorasa"
+                    }
+                }
+                scm {
+                    connection = "https://github.com/csutorasa/wiclax4j.git"
+                    developerConnection = "https://github.com/csutorasa/wiclax4j.git"
+                    url = "https://github.com/csutorasa/wiclax4j"
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+jacoco {
+    toolVersion = "0.8.7"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+    }
+}
+
+sonarqube {
+    properties {
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.organization", "csutorasa")
+        property("sonar.branch.name", "master")
+        property("sonar.login", System.getenv("SONAR_TOKEN"))
+        property("sonar.sourceEncoding", "UTF-8")
+    }
+}
+project.tasks["sonarqube"].dependsOn "jacocoTestReport"
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "wiclax4j"
